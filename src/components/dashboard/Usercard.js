413d01bd9df47bca61a1d15d99165e709fb376fd
aakash1122 <aakaashhossain@gmail.com>
2019-02-28 22:07:07 +0600

    updated user profile card design

--- src/components/dashboard/Usercard.js
+++ src/components/dashboard/Usercard.js
@@ -2,42 +2,19 @@ import React from "react";
 
 export default function Usercard({ user }) {
   return (
-    <div className="div container d-flex flex-column">
-      <div className="profile-container">
-        <div className="row">
-          <div className="col-md-4">
-            <img
-              src="https://images.pexels.com/photos/1569076/pexels-photo-1569076.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260"
-              alt=""
-              className="w-100 h-100 user-image p-3"
-            />
-          </div>
-          <div className="col-md-8 profile-info d-flex flex-column justify-content-center">
-            <h2>{user.name}</h2>
-            <p>Username : {user.username}</p>
-            <p>{user.email}</p>
-            <p className="mt-2">
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Quae ipsa
-              error, iste expedita magnam incidunt minima officia? A saepe,
-            </p>
-          </div>
-        </div>
+    <div className="user-image-card container text-dark">
+      <div className="imagebox left">
+        <img
+          src="https://images.pexels.com/photos/415829/pexels-photo-415829.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260"
+          alt=""
+          className="userImage"
+        />
       </div>
-      <div className="social-links mx-auto mt-2 p-2">
-        <span className="fa-stack fa-2x m-2">
-          <i className="fas fa-circle fa-stack-2x" />
-          <i className="fab fa-instagram fa-stack-1x fa-inverse" />
-        </span>
-        <span className="fa-stack fa-2x m-2">
-          <i className="fas fa-circle fa-stack-2x" />
-          <i className="fab fa-facebook-f fa-stack-1x fa-inverse" />
-        </span>
-        <span className="fa-stack fa-2x m-2">
-          <i className="fas fa-circle fa-stack-2x" />
-          <i className="fab fa-linkedin fa-stack-1x fa-inverse" />
-        </span>
+      <div className="userinfo right">
+        <h3>{user.name}</h3>
+        <p>{user.email}</p>
+        <p>Username: {user.name}</p>
       </div>
-      <hr />
     </div>
   );
 }
